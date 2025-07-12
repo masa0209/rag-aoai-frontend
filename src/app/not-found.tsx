d4d749b6c5fe6878bb72b9d3b06d1c976fe522aa
masa0209 <masaharu.watabe.k2w@gmail.com>
2025-07-12 21:10:25 +0900

    Actionsを動かすため！

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ const NotFound = () => {
             <h1 className="text-8xl font-bold">404</h1>
             <p className="text-4xl font-medium">Page not found</p>
             <Link href="/" className="text-blue-500 mt-4 text-xl hover:underline">
-                Go Back Page
+                Go Back Page!
             </Link>
         </div>
     )
